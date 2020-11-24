cb42f7cb47f38f234a1f4515d7e9014ddf216b21
ziveo <bzivkovic@blueraster.com>
2020-11-23 22:17:04 -0500

    Adding try/catch on initializeMap()

--- src/controller/MapController.js
+++ src/controller/MapController.js
@@ -13,26 +13,30 @@ class MapController {
   }
 
   initializeMap = async (setCoordinate) => {
-    this.setCoordinate = setCoordinate;
-    const [Map, MapView] = await loadModules(['esri/Map', 'esri/views/MapView']);
-    this._map = new Map({
-      basemap: 'gray-vector',
-    });
-
-    this._mapView = new MapView({
-      map: this._map,
-      container: 'mapContainer',
-      basemap: 'gray-vector',
-      center: [-100, 30],
-      zoom: 5,
-    });
-
-    this._mapView.on('click', (evt) => {
-      this.setCoordinate({
-        lat: evt.mapPoint.latitude,
-        long: evt.mapPoint.longitude,
+    try {
+      this.setCoordinate = setCoordinate;
+      const [Map, MapView] = await loadModules(['esri/Map', 'esri/views/MapView']);
+      this._map = new Map({
+        basemap: 'gray-vector',
       });
-    });
+
+      this._mapView = new MapView({
+        map: this._map,
+        container: 'mapContainer',
+        basemap: 'gray-vector',
+        center: [-100, 30],
+        zoom: 5,
+      });
+
+      this._mapView.on('click', (evt) => {
+        this.setCoordinate({
+          lat: evt.mapPoint.latitude,
+          long: evt.mapPoint.longitude,
+        });
+      });
+    } catch (e) {
+      console.log(e);
+    }
   };
 }
 
